38c883dfc53152bab034742fb573f180dba180a2
Baptiste Jouk <baptistejouk@macbook-pro-de-baptiste.home>
2017-05-02 20:35:05 +0200

    with homepage

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express=require('express')
 var app=express()
+app.set('view engine', 'pug');
 var urlDb = 'mongodb://localhost:27017/linksapi';
 var mongo=require('mongodb').MongoClient
 function checkUrl(url){
@@ -16,14 +17,15 @@ function editUrl(url){
 	}
 	return url
 }
+
 app.get('/new/:url',function(req,res){
+	console.log(req.params.url)
 	mongo.connect(urlDb, function(err, db) {
 	    var collection = db.collection('links')
 	    if(checkUrl){
 	    	collection.insert({url:editUrl(req.params.url)},function(err,docsInserted){
-	    		res.json({url:req.params.url,shortened:'http://localhost:8080/'+docsInserted["ops"][0]["_id"]})
-	    	})
-			
+	    		res.json({url:req.params.url,shortened:'http://localhost:8080/retrieve/'+docsInserted["ops"][0]["_id"]})
+	    	})	
 	    }
 	    else{
 	    	res.json({error:'Not an url'})
@@ -32,16 +34,20 @@ app.get('/new/:url',function(req,res){
 		db.close();
 		})
  	})
-
-app.get('/:id',function(req,res){
+app.get('/',function(req,res){
+	res.render('./template.pug',{title:'URL Shortener',message:'URL Shortener',instructions:'To shorten URL call /new/<YOUR URL>'})
+})
+app.get('/retrieve/:id',function(req,res){
 	mongo.connect(urlDb, function(err, db) {
-	    var collection = db.collection('links')
+		var collection = db.collection('links')
 		collection.find({index:parseInt(req.params.id)}).toArray(function(err,results){
 			var urlRedirect=results[0]['url']
 			res.redirect(urlRedirect)
 			res.end();
 		})
 		db.close();
-		})	
+	})	
 })
+
+
 app.listen(8080);
